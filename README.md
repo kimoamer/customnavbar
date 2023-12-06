314e84c124602b2fea6d86c2acba222fcc2fa139
kimoamer <farisamer2020@gmail.com>
2023-12-06 22:24:13 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Customnavbar
+
+Navbar
+
+#### License
+
+MIT
---
+++ customnavbar/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Customnavbar",
+			"type": "module",
+			"label": _("Customnavbar")
+		}
+	]
